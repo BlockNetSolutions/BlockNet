7772c4919b5c1cc8012d1aae59a399a5bcb6e88d
Johann <johannjudek@icloud.com>
2023-04-14 21:59:48 +0200

    add logger

--- Cloud/src/main/java/de/blocknet/cloud/Main.java
+++ Cloud/src/main/java/de/blocknet/cloud/Main.java
@@ -6,24 +6,34 @@ import org.jline.reader.LineReaderBuilder;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
 
+import java.io.Console;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 public class Main {
 
     private static SetupManager setupManager;
     private static boolean isSetup;
 
+
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) throws IOException {
         setup();
+
 
         Terminal terminal = TerminalBuilder.terminal();
         LineReader lineReader = LineReaderBuilder.builder()
                 .terminal(terminal)
                 .build();
 
+
+
+
+
         String line;
         while ((line = lineReader.readLine("BlockNet > ")) != null) {
-            System.out.println("Du hast eingetippt: " + line);
+            logger.info("Du hast eingetippt: " + line);
         }
     }
 
