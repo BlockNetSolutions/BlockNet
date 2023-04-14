26d4f3d3a751005716612c528cda1f77cd3816f6
Johann <johannjudek@icloud.com>
2023-04-14 21:46:17 +0200

    update gradle for cloud

--- Cloud/src/main/java/de/blocknet/cloud/Main.java
+++ Cloud/src/main/java/de/blocknet/cloud/Main.java
@@ -1,15 +1,33 @@
 package de.blocknet.cloud;
 
 import de.blocknet.setup.SetupManager;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+
+import java.io.IOException;
 
 public class Main {
 
     private static SetupManager setupManager;
     private static boolean isSetup;
 
-    public static void main(String[] args) {
-        System.out.println("test");
+    public static void main(String[] args) throws IOException {
         setup();
+
+
+
+
+        Terminal terminal = TerminalBuilder.terminal();
+        LineReader lineReader = LineReaderBuilder.builder()
+                .terminal(terminal)
+                .build();
+
+        String line;
+        while ((line = lineReader.readLine("> ")) != null) {
+            System.out.println("You typed: " + line);
+        }
     }
 
     private static void setup() {
