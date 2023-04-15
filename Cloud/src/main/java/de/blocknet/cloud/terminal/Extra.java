3869312e19875d7b26c22602bdd993a313a8e87f
Johann <johannjudek@icloud.com>
2023-04-15 21:47:17 +0200

    update build.gradle

--- Cloud/src/main/java/de/blocknet/cloud/terminal/Extra.java
+++ Cloud/src/main/java/de/blocknet/cloud/terminal/Extra.java
@@ -25,6 +25,7 @@ public class Extra {
                         "                                                                          \n" +
                         "                                                                          \n" +
                         "                                                                          ");
+        terminal.writer().println(System.getProperty("os"));
     }
 
 }
